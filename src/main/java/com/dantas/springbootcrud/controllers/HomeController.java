ec852ca877041684124fab50fa29727d24d9f06d
pa-tiq <patrickdsilva99@gmail.com>
2023-05-02 14:52:31 -0400

    initial

---
+++ src/main/java/com/dantas/springbootcrud/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.dantas.springbootcrud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // define essa classe como um controlador MVC
+public class HomeController {
+    @GetMapping(path="/")
+    public String index(Model model){
+        model.addAttribute("nome","Dantas");
+        return "home/index.html";
+    }
+
+}
